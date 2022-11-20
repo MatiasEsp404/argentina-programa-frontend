dd55d9779e07c1550172a15bac4b7701c99a360e
GaspariFernando <dev.gaspari.fernando@gmail.com>
2022-11-19 23:13:38 -0300

    patch information

--- src/app/components/validation/validation.component.ts
+++ src/app/components/validation/validation.component.ts
@@ -1,7 +1,10 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Information } from "../../models/information/information";
 import { User } from "../../models/validation/user";
 import { ValidationService } from "../../services/validation/validation.service";
+import { Token } from "../../models/validation/token";
+
 
 @Component({
   selector: 'app-validation',
@@ -32,10 +35,14 @@ export class ValidationComponent implements OnInit {
   };
 
   public user: User = {
-    email: '',
-    password: ''
+    email: 'foo@outlook.com',
+    password: 'ArgProg2022'
   }
 
+  public token: Token;
+
+
+  public userInvalid: Boolean = true;
 
   public loginDisabled: Boolean = false;
   public btnLoginDisabled: Boolean = true;
@@ -54,11 +61,12 @@ export class ValidationComponent implements OnInit {
   }
 
   loggedIn(event: Event){
-    this.btnLoginDisabled = false;
-    this.loginDisabled = !this.loginDisabled;
-    this.logged = true;
+    
     console.log("en la variable user: " + this.user.email + " " + this.user.password);
-    this.authUser()
+    this.authUser();
+    console.log(this.userInvalid);
+    
+
   }
 
   editInfo(){
@@ -71,7 +79,8 @@ export class ValidationComponent implements OnInit {
   }
 
   submit(){
-    console.log(this.information)
+    console.log(this.information);
+    this.patchInfo();
   }
 
   changeError(){
@@ -80,8 +89,32 @@ export class ValidationComponent implements OnInit {
   
   private authUser(){
     this.validationService.authUser(this.user).subscribe(info => {
-      this.user = info;
-      console.log(info);
+      console.log('esta es la respuesta del endpoint' + info);
+      console.log(info.toString());
+
+      
+      
+      try {
+        this.token = info;
+        
+      } catch (error) {
+        console.log('No se guardo token en la variable');
+        
+      }
+      console.log('este es el token: ' + this.token);
+      this.btnLoginDisabled = false;
+      this.loginDisabled = !this.loginDisabled;
+      this.logged = true;
+      this.userInvalid = false;
+      console.log(this.userInvalid);
+    })
+  }
+
+  private patchInfo(){
+    this.validationService.patchInfo(this.information, this.token.token).subscribe(info => {
+      this.information = info;
+      console.log(this.information);
+      
       
     })
   }
--- src/app/services/validation/validation.service.ts
+++ src/app/services/validation/validation.service.ts
@@ -1,18 +1,31 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Information } from 'src/app/models/information/information';
 import { User } from '../../models/validation/user';
+import { Token } from "../../models/validation/token";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ValidationService {
 
-  private baseURL = "http://localhost:8080/api/auth/login";
+  private basePostURL = "http://localhost:8080/api/auth/login";
+  private basePatchURL = "http://localhost:8080/api/information";
 
   constructor(private httpClient : HttpClient) { }
 
   authUser(user: User){
-    return this.httpClient.post<User>(`${this.baseURL}`, user);
+    return this.httpClient.post<Token>(`${this.basePostURL}`, user);
+  }
+
+  patchInfo(body: Information, token: string){
+
+    const headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': token
+    })
+
+    return this.httpClient.patch<Information>(`${this.basePatchURL}`, body, {'headers': headers});
   }
 }
