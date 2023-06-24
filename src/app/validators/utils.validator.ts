29b6f31f2418a81b094464fbf560ca0880ae8722
MatiasEsp404 <unity.matias@outlook.com>
2023-06-24 20:11:06 -0300

    Corregido error con fechas y zona horaria

--- src/app/validators/utils.validator.ts
+++ src/app/validators/utils.validator.ts
@@ -1,11 +1,12 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+import * as moment from 'moment';
 
 export function menorIgualAFechaActual(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
     if (control.value) {
-      const fechaIngresada = new Date(control.value)
-      const fechaActual = new Date()
-      if (fechaIngresada > fechaActual) {
+      const fechaIngresada = moment.utc(control.value, 'YYYY-MM-DD')
+      const fechaActual = moment.utc().startOf('day')
+      if (fechaIngresada.isAfter(fechaActual)) {
         return { fechaNoValida: true }
       }
     }
